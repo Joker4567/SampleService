fe1d96842c1e7e77348062fff82566c89ef946e2
danufriyev <W5n@$0scByt4>
2021-04-11 20:24:07 +0500

    push alarmManager

--- core-ui/src/main/java/pro/enaza/core_ui/activity/MainActivity.kt
+++ core-ui/src/main/java/pro/enaza/core_ui/activity/MainActivity.kt
@@ -1,9 +1,15 @@
 package pro.enaza.core_ui.activity
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.ComponentName
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import pro.enaza.core_ui.R
+import pro.enaza.feature_lock.receiver.BootBroadcastReceiver
 import pro.enaza.feature_lock.service.LockService
 
 @AndroidEntryPoint
@@ -11,5 +17,40 @@ class MainActivity : AppCompatActivity(R.layout.main_activity) {
     override fun onStart() {
         super.onStart()
         startService(Intent(this, LockService::class.java))
+        registerReceiverAlarm()
+        registerAlarm()
+    }
+
+    private fun registerAlarm() {
+        val receiver = ComponentName(this, BootBroadcastReceiver::class.java)
+
+        packageManager.setComponentEnabledSetting(
+                receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP
+        )
+    }
+
+    private fun registerReceiverAlarm() {
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+
+        val alarmIntent = Intent(this, BootBroadcastReceiver::class.java)
+                .apply {
+                    addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
+                    addFlags(Intent.FLAG_RECEIVER_FOREGROUND)
+                }
+        var pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0)
+
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> {
+                alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, 0, pendingIntent)
+            }
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT -> {
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, 0, pendingIntent)
+            }
+            else -> {
+                alarmManager.set(AlarmManager.RTC_WAKEUP, 0, pendingIntent)
+            }
+        }
     }
 }
